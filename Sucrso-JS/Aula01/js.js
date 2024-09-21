d7c987c9cd9c6f21f16efa884c4260ff1c79fcb4
Jonhy Santos <jonhy1905@hotmail.com>
2024-09-21 16:02:06 -0300

    pratica do dia.

---
+++ Sucrso-JS/Aula01/js.js
@@ -0,0 +1,9 @@
+const estoque = []
+
+
+function cadastrarItem(){
+    const nome = prompt('Digite o nome do item que deseja cadastrar ao estoque')
+    const quantidade = parseInt(prompt('Digite a quantidade do item:'))
+    const item = {}  
+    
+}
