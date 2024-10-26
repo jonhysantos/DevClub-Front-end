b3fe07482534b1d387d9d3e2181d41cf43ee354f
Jonhy Santos <jonhy1905@hotmail.com>
2024-10-25 23:48:11 -0300

    pratica do map.

---
+++ Curso-JS-05/Aula-01-map/practice06.js
@@ -0,0 +1,26 @@
+const list = [
+    {name:'jonhy',age:'36',vip:true},
+    {name:'aninha',age:'10',vip:true},
+    {name:'Priscila',age:'37',vip:true},
+    {name:'Cecilia',age:'37',vip:false},
+    {name:'iza',age:'37',vip:false},
+
+]
+
+const vipOrNot = list.map(user => {
+    const vip = {
+        name:user.name,
+        age:user.age,
+        braceletColor:user.vip ? 'black' : 'Red'
+    }
+    return vip
+})
+
+const students = [
+    {name:'jonhy',age:'36',test:8},
+    {name:'aninha',age:'10',test:10},
+    {name:'Priscila',age:'37',test:4},
+    {name:'Cecilia',age:'37',test:2},
+    {name:'iza',age:'37',test:2},
+]
+
