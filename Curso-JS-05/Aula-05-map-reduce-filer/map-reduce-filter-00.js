4d57195c60751390a92beb29076475f01bf2e796
Jonhy Santos <jonhy1905@hotmail.com>
2024-11-02 10:05:36 -0300

    pratica do map, reduce e filter.

---
+++ Curso-JS-05/Aula-05-map-reduce-filer/map-reduce-filter-00.js
@@ -0,0 +1,27 @@
+const companies = [
+    {name:'samsung',marketValue: 50,CEO:'Kim hyum suk',foundedOn:1938},
+    {name:'Microsoft',marketValue: 415,CEO:'Satya Nadella',foundedOn:1975},
+    {name:'Intel',marketValue: 117,CEO:'Brian Krzanich',foundedOn:1968},
+    {name:'Facebook',marketValue:383,CEO:'Mark zuckerberg',foundedOn:2004},
+    {name:'Spotify',marketValue:30,CEO:'Daniel ek',foundedOn:2006},
+    {name:'Apple',marketValue:845,CEO:'Tim cook',foundedOn:1976},
+]
+
+    // Adicionar 10% de valor de mercado as todas as companias -> MAP
+    // Filtrar somente as companias fundada abaixo de 1990 -> FILTER
+    // Somar o valor de mercado das restantes -> REDUCE
+
+const add10Percente = (company => {
+    company.marketValue = company.marketValue + (company.marketValue * 5)
+    return company
+})
+
+const filterComapanies = (company => company.foundedOn < 1990 )
+
+const calculateTotalMarketValue = (acumulate,company) => acumulate + company.marketValue
+
+const marketValueOldCompanies = companies.map(add10Percente)
+    .filter(filterComapanies)
+    .reduce(calculateTotalMarketValue,0)
+
+console.log(marketValueOldCompanies)
---
+++ Curso-JS-05/Aula-05-map-reduce-filer/map-reduce-filter-02.js
@@ -0,0 +1,12 @@
+const companies = [
+    {name:'samsung',marketValue: 50,CEO:'Kim hyum suk',foundedOn:1938},
+    {name:'Microsoft',marketValue: 415,CEO:'Satya Nadella',foundedOn:1975},
+    {name:'Intel',marketValue: 117,CEO:'Brian Krzanich',foundedOn:1968},
+    {name:'Facebook',marketValue:383,CEO:'Mark zuckerberg',foundedOn:2004},
+    {name:'Spotify',marketValue:30,CEO:'Daniel ek',foundedOn:2006},
+    {name:'Apple',marketValue:845,CEO:'Tim cook',foundedOn:1976},
+]
+
+    // Adicionar 10% de valor de mercado as todas as companias -> MAP
+    // Filtrar somente as companias fundada abaixo de 1990 -> FILTER
+    // Somar o valor de mercado das restantes -> REDUCE
