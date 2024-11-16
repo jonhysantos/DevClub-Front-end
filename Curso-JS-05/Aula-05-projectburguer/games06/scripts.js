9f85447a621adec15c0edccbabaf34b0500a9e90
Jonhy Santos <jonhy1905@hotmail.com>
2024-11-16 17:03:36 -0300

    pratica

--- Curso-JS-05/Aula-05-projectburguer/games06/scripts.js
+++ Curso-JS-05/Aula-05-projectburguer/games06/scripts.js
@@ -1,24 +1,3 @@
 import {games} from './products.js'
 
-
 const btnShowAll = document.querySelector('.show-all');
-const list = document.querySelector('ul')
-
-function showAll(arrayProduct){
-    let myLi = ''
-    arrayProduct.forEach(product => {
-        myLi += `
-            <li>
-                <img src=${product.src}>
-                <p>${product.name}</p>
-                <p class="item-price">${product.price}</p>
-            </li>
-            
-            
-        `
-    })
-    list.innerHTML = myLi
-
-}
-
-btnShowAll.addEventListener('click',() => showAll(games))
